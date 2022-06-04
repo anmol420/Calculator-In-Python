72a29e89ca89b62ef2f3cc7b09c45713b47f4010
Anmol Anand <anmolanand2k15@gmail.com>
2022-06-04 14:32:42 +0530

    Added 10% Of The Whole

---
+++ app.py
@@ -0,0 +1,59 @@
+from ast import operator
+import tkinter
+from tkinter import *
+from tkinter import messagebox
+
+val = ""
+A = 0
+operator = ""
+
+def btn_1_clicked():
+    global val
+    val = val + "1"
+    data.set(val)
+
+def btn_2_clicked():
+    global val
+    val = val + "2"
+    data.set(val)
+
+def btn_3_clicked():
+    global val
+    val = val + "3"
+    data.set(val)
+
+def btn_4_clicked():
+    global val
+    val = val + "4"
+    data.set(val)
+
+def btn_5_clicked():
+    global val
+    val = val + "5"
+    data.set(val)
+
+def btn_6_clicked():
+    global val
+    val = val + "6"
+    data.set(val)
+
+def btn_7_clicked():
+    global val
+    val = val + "7"
+    data.set(val)
+
+def btn_8_clicked():
+    global val
+    val = val + "8"
+    data.set(val)
+
+def btn_9_clicked():
+    global val
+    val = val + "9"
+    data.set(val)
+
+def btn_0_clicked():
+    global val
+    val = val + "0"
+    data.set(val)
+
